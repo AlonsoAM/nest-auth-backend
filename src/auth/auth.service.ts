e95af1bdff175e337108e7b4d0ffade02ba5b511
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-05-24 12:40:39 -0500

    Crear un registro en la base de datos

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    return 'This action adds a new auth';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const newUser = new this.userModel(createUserDto);
+
+      // 1. Encriptar la contraseña
+
+      // 2. Guardar el usuario
+
+      // 3. GEnerar el JWT
+
+      return await newUser.save();
+    } catch (error) {
+      // 4. Manejar los errores
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} ya existe!`);
+      }
+      throw new InternalServerErrorException('Algo terrible sucedió!');
+    }
   }
 
   findAll() {
