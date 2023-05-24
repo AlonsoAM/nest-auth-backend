d322c0f4c375c78ca12a8c50d7942fe2a3ca1719
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-05-24 12:50:50 -0500

    Encriptar la contraseña

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcryptjs from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -15,15 +16,21 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
-      const newUser = new this.userModel(createUserDto);
-
+      const { password, ...userData } = createUserDto;
       // 1. Encriptar la contraseña
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
 
       // 2. Guardar el usuario
+      await newUser.save();
 
-      // 3. GEnerar el JWT
+      // 3. Quitar el password del objeto que retorna como respuesta
+      const { password: _, ...user } = newUser.toJSON();
 
-      return await newUser.save();
+      // 4. Reotornar usuario
+      return user;
     } catch (error) {
       // 4. Manejar los errores
       if (error.code === 11000) {
